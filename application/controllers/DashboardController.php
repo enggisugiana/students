9df1f482cac14a910c993a32151ce56542021940
enggisugiana <enggi.sugiana@gmail.com>
2020-10-22 10:13:42 +0700

    student function save

--- application/controllers/DashboardController.php
+++ application/controllers/DashboardController.php
@@ -7,6 +7,7 @@ class DashboardController extends CI_Controller
     {
         parent::__construct();
         $this->load->model('CampusModel');
+        $this->load->model('StudentModel');
     }
 
     public function index()
@@ -17,7 +18,8 @@ class DashboardController extends CI_Controller
         $this->load->view('dashboard/layouts/footer');
     }
 
-    public function add_campus() {
+    public function add_campus()
+    {
         $this->load->view('dashboard/layouts/navbar');
         $this->load->view('dashboard/layouts/sidebar');
         $this->load->view('dashboard/campus/addCampus');
@@ -29,21 +31,19 @@ class DashboardController extends CI_Controller
         $logo = $_FILES['logo']['name'];
         $extension = pathinfo($_FILES['logo']['name'], PATHINFO_EXTENSION);
         $date = date('Ymdhis');
-        $filename = $date. "." . $extension;
-        
+        $filename = $date . "." . $extension;
+
         $config['upload_path'] = "assets/images/";
         $config['allowed_types'] = "jpg|jpeg|png";
         $config['file_name'] = $filename;
         $config['remove_spaces'] = TRUE;
 
-        $this->load->library('upload',$config);
+        $this->load->library('upload', $config);
 
-        if( ! $this->upload->do_upload('logo'))
-        {
+        if (!$this->upload->do_upload('logo')) {
             $this->session->set_flashdata('error_up_foto', 'value');
             redirect(site_url('dashboard/addformcampus'));
-        }
-        else{
+        } else {
             $logo = $filename;
         }
 
@@ -60,7 +60,8 @@ class DashboardController extends CI_Controller
         redirect(site_url('dashboard/addcampus'));
     }
 
-    public function list_campus() {
+    public function list_campus()
+    {
         $this->load->view('dashboard/layouts/navbar');
         $this->load->view('dashboard/layouts/sidebar');
         $this->load->view('dashboard/campus/listCampus');
@@ -74,43 +75,41 @@ class DashboardController extends CI_Controller
     }
 
     public function get_detail_kampus()
-	{
-		$id = $this->input->post('id');
+    {
+        $id = $this->input->post('id');
         $res = $this->CampusModel->get_kampus_by_id($id);
-		echo json_encode($res);
+        echo json_encode($res);
     }
 
     public function update_detail_kampus()
-	{   
+    {
         $date_now = date('Y-m-d H:i:s');
 
         $logo_lama = $this->input->post('logo_lama');
-        
+
         $logo = $_FILES['logo']['name'];
         $extension = pathinfo($_FILES['logo']['name'], PATHINFO_EXTENSION);
         $date = date('Ymdhis');
-        $filename = $date. "." . $extension;
+        $filename = $date . "." . $extension;
 
-        if($logo) {
+        if ($logo) {
             // echo 'gk msk gambar';die();
-            unlink('assets/images/'.$logo_lama);
+            unlink('assets/images/' . $logo_lama);
 
             $config['upload_path'] = "assets/images/";
             $config['allowed_types'] = "jpg|jpeg|png";
             $config['file_name'] = $filename;
             $config['remove_spaces'] = TRUE;
 
-            $this->load->library('upload',$config);
+            $this->load->library('upload', $config);
 
-            if( ! $this->upload->do_upload('logo'))
-            {
+            if (!$this->upload->do_upload('logo')) {
                 $this->session->set_flashdata('error_up_foto', 'value');
                 redirect(site_url('dashboard/listcampus'));
-            }
-            else{
+            } else {
                 $logo = $filename;
             }
-            $data = array (
+            $data = array(
                 'nama' => $this->input->post('nama'),
                 'alamat' => $this->input->post('alamat'),
                 'provinsi' => $this->input->post('provinsi'),
@@ -121,9 +120,8 @@ class DashboardController extends CI_Controller
             $this->db->where('id', $this->input->post('id'));
             $res = $this->db->update('kampus', $data);
             echo json_encode($res);
-
         } else {
-            $data = array (
+            $data = array(
                 'nama' => $this->input->post('nama'),
                 'alamat' => $this->input->post('alamat'),
                 'provinsi' => $this->input->post('provinsi'),
@@ -135,12 +133,12 @@ class DashboardController extends CI_Controller
             echo json_encode($res);
         }
     }
-    
-    public function delete_kampus() 
-	{   
+
+    public function delete_kampus()
+    {
         $id = $this->input->post('id');
         $logo = $this->input->post('logo_hapus');
-        unlink('assets/images/'.$logo);
+        unlink('assets/images/' . $logo);
 
         $this->db->where('id', $id);
         $res = $this->db->delete('kampus');
@@ -154,5 +152,43 @@ class DashboardController extends CI_Controller
         $this->load->view('dashboard/layouts/sidebar');
         $this->load->view('dashboard/pages/campus');
         $this->load->view('dashboard/layouts/footer');
+    }
+
+    // Form student code by Enggi Sugiana
+
+    public function students()
+    {
+        $this->load->view('dashboard/layouts/navbar');
+        $this->load->view('dashboard/layouts/sidebar');
+        $this->load->view('dashboard/pages/students');
+        $this->load->view('dashboard/layouts/footer');
+    }
+
+    public function submitFormStudent()
+    {
+        $data = array(
+            'id_kampus' => $this->input->post('id_kampus'),
+            'nama' => $this->input->post('nama'),
+            'email' => $this->input->post('email'),
+            'alamat' => $this->input->post('alamat'),
+            'jurusan' => $this->input->post('jurusan'),
+            'fakultas' => $this->input->post('fakultas'),
+            'angkatan' => $this->input->post('angkatan'),
+            'basic_storage' => $this->input->post('basic_storage'),
+        );
+
+        $result = $this->db->insert('mahasiswa', $data);
+        $msg['success'] = false;
+        if ($result) {
+            $msg['success'] = true;
+        }
+
+        echo json_encode($msg);
+    }
+
+    public function get_all_student()
+    {
+        $result = $this->StudentModel->get_all_students();
+        echo json_encode($result);
     }
 }
--- application/views/dashboard/pages/students.php
+++ application/views/dashboard/pages/students.php
@@ -19,44 +19,28 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <button class="btn btn-primary" id="modal-1" data-toggle="modal" data-target="#addStudent">Add Data</button>
+                            <button class="btn btn-lg btn-primary" id="modal-1" data-toggle="modal" data-target="#addStudent" style="border-radius: .25rem;"><i class="fas fa-plus"></i> Add Data</button>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-striped" id="table-1">
+                                <table class="table table-striped" id="datatable">
                                     <thead>
                                         <tr>
                                             <th class="text-center">
-                                                #
+                                                No
                                             </th>
-                                            <th>Nama</th>
+                                            <th>Kampus</th>
+                                            <th>Nama Mahasiswa</th>
+                                            <th>Email</th>
                                             <th>Alamat</th>
-                                            <th>Members</th>
-                                            <th>Due Date</th>
-                                            <th>Status</th>
-                                            <th>Action</th>
+                                            <th>Jurusan</th>
+                                            <th>Fakultas</th>
+                                            <th>Angkatan</th>
+                                            <th>Basic Storage</th>
+                                            <th class="text-center">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>
-                                                1
-                                            </td>
-                                            <td>Create a mobile app</td>
-                                            <td class="align-middle">
-                                                <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
-                                                    <div class="progress-bar bg-success" data-width="100%"></div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <img alt="image" src="<?= base_url('assets/img/avatar/avatar-5.png'); ?>" class="rounded-circle" width="35" data-toggle="tooltip" title="Wildan Ahdian">
-                                            </td>
-                                            <td>2018-01-20</td>
-                                            <td>
-                                                <div class="badge badge-success">Completed</div>
-                                            </td>
-                                            <td><a href="#" class="btn btn-secondary">Detail</a></td>
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
@@ -70,11 +54,11 @@
 </div>
 <!-- #Modal -->
 <div class="modal fade" tabindex="-1" role="dialog" id="addStudent">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
-            <form class="needs-validation" novalidate="">
+            <form action="" method="post" class="needs-validation" novalidate="" id="formStudent">
                 <div class="modal-header">
-                    <h5 class="modal-title">Mahasiswa</h5>
+                    <h5 class="modal-title">Input Data Mahasiswa</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -82,66 +66,144 @@
                 <div class="modal-body">
                     <div class="form-group">
                         <label>Nama Kampus</label>
-                        <input type="text" class="form-control" required="" placeholder="Masukan nama kampus">
+                        <input type="text" name="id_kampus" class="form-control" required="" placeholder="Masukan nama kampus" id="id_kampus">
                         <div class="invalid-feedback">
-                            What's your name?
+                            Nama Kampus tidak boleh kosong!
                         </div>
                     </div>
                     <div class="form-group">
                         <label>Nama Mahasiswa</label>
-                        <input type="text" class="form-control" required="" placeholder="Masukan nama mahasiswa">
+                        <input type="text" name="nama" class="form-control" required="" placeholder="Masukan nama mahasiswa">
                         <div class="invalid-feedback">
-                            What's your name?
+                            Nama mahasiswa tidak boleh kosong
                         </div>
                     </div>
                     <div class="form-group">
                         <label>Email</label>
-                        <input type="email" class="form-control" required="" placeholder="Masukan email">
+                        <input type="email" name="email" class="form-control" required="" placeholder="Masukan email">
                         <div class="invalid-feedback">
                             Email yang dimasukan invalid.
                         </div>
                     </div>
                     <div class="form-group mb-0">
                         <label>Alamat</label>
-                        <textarea class="form-control" required="" row="10" placeholder="Masukan alamat"></textarea>
+                        <textarea class="form-control" name="alamat" required="" row="10" placeholder="Masukan alamat"></textarea>
                         <div class="invalid-feedback">
                             Alamat tidak boleh kosong?
                         </div>
                     </div>
                     <div class="form-group">
                         <label>Jurusan</label>
-                        <input type="text" class="form-control" required="" placeholder="Masukan jurusan">
+                        <input type="text" name="jurusan" class="form-control" required="" placeholder="Masukan jurusan">
                         <div class="invalid-feedback">
-                            What's your name?
+                            Jurusan tidak boleh kosong!
                         </div>
                     </div>
                     <div class="form-group">
                         <label>Fakultas</label>
-                        <input type="text" class="form-control" required="" placeholder="Masukan fakultas">
+                        <input type="text" name="fakultas" class="form-control" required="" placeholder="Masukan fakultas">
                         <div class="invalid-feedback">
-                            What's your name?
+                            Fakultas tidak boleh kosong!
                         </div>
                     </div>
                     <div class="form-group">
                         <label>Angkatan</label>
-                        <input type="text" class="form-control" required="" placeholder="Masukan angkatan">
+                        <input type="text" name="angkatan" class="form-control" required="" placeholder="Masukan angkatan">
                         <div class="invalid-feedback">
-                            What's your name?
+                            Angkatan tidak boleh kosong!
                         </div>
                     </div>
                     <div class="form-group">
                         <label>Basic Storage</label>
-                        <input type="text" class="form-control" required="" placeholder="Masukan kapasitas storage">
+                        <input type="text" nama="basic_storage" class="form-control" required="" placeholder="Masukan kapasitas storage">
                         <div class="invalid-feedback">
-                            What's your name?
+                            Basic storage tidak boleh kosong!
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer bg-whitesmoke br">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <button type="submit" class="btn btn-primary" id="btnSimpan">Simpan</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        var t = $('#datatable').DataTable({
+            buttons: [
+                'excel', 'pdf'
+            ]
+        });
+
+        $('#formStudent').attr('action', '<?= base_url('DashboardController/submitFormStudent'); ?>');
+        $('#formStudent').submit(function() {
+            var url = $('#formStudent').attr('action');
+            var data = $('#formStudent').serialize();
+
+            $.ajax({
+                type: 'ajax',
+                method: 'post',
+                url: url,
+                data: data,
+                async: false,
+                dataType: 'json',
+                success: function(response) {
+                    if (response.success) {
+                        clearForm();
+                        $('#addStudent').modal('toggle');
+                        getStudents();
+                    } else {
+                        alert('Error');
+                    }
+                },
+                error: function() {
+                    alert('Data cannot be save');
+                }
+            });
+        });
+
+        function clearForm() {
+            $('#id_kampus').val("");
+        }
+
+        getStudents();
+
+        function getStudents() {
+            $.ajax({
+                url: "<?php echo base_url() ?>DashboardController/get_all_student",
+                type: "POST",
+                data: null,
+                success: function(ajaxData) {
+                    t.clear().draw();
+                    var result = JSON.parse(ajaxData);
+
+                    for (var i = 0; i < result.length; i++) {
+                        var button1 = "<a href='#' class='btn-edit' data-id='" + result[i]['id'] + "' title='Edit' style='color:#1ABB9C;'><span class='fa fa-edit fa-2x'></span></a>";
+                        var button2 = "<a href='#' class='btn-hapus' data-id='" + result[i]['id'] + "' data-nama='" + result[i]['nama'] + "' data-logo='" + result[i]['logo'] + "' title='Hapus' style='color:#bb1a1a;'><span class='fa fa-trash fa-2x'></span></a>";
+
+                        t.row.add([
+                            i + 1,
+                            result[i]['id_kampus'],
+                            result[i]['nama'],
+                            result[i]['email'],
+                            result[i]['alamat'],
+                            result[i]['jurusan'],
+                            result[i]['fakultas'],
+                            result[i]['angkatan'],
+                            result[i]['basic_storage'],
+                            "<center>" + button1 + " " + button2 + "</center>",
+
+                        ]).draw();
+                    }
+
+                },
+                error: function(status) {
+                    t.clear().draw();
+                }
+            });
+        }
+    });
+</script>
